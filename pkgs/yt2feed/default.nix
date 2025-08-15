a9ee13389b65ed76894c7d2630a4711a695cf896
Thomas Koch <thomas@koch.ro>
2025-08-15 15:16:37 +0300

    bump yt2feed version

--- pkgs/yt2feed/default.nix
+++ pkgs/yt2feed/default.nix
@@ -6,14 +6,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "yt2feed";
-  version = "0.2";
+  version = "0.2.1";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "thkoch2001";
     repo = "${pname}";
     rev = "v${version}";
-    hash = "sha256-x1pB7GSb0cgCQ5goKGeUxRNE/tWfYbqa5hhNlR+Pits=";
+    hash = "sha256-uspZY42IREiELap0Aht39R3fgFxFi1mQsF4XpPqBTdk=";
   };
 
   buildInputs = with python3Packages; [
